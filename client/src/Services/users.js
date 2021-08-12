fb232f9f722115178cf2a83f1310069bfa7306da
Jessa Clark <84357702+jessa-clark@users.noreply.github.com>
2021-08-12 12:10:14 -0400

    set up Services folder set up config.js set up users.js

---
+++ client/src/Services/users.js
@@ -0,0 +1,30 @@
+import api from './config';
+
+export const login = async (loginData) => {
+	const res = await api.post('/users/login', { user: loginData });
+	localStorage.setItem('authToken', res.data.token);
+	api.defaults.headers.common.authorization = `Bearer ${res.data.token}`;
+	return res.data.user;
+};
+
+export const register = async (registerData) => {
+	const res = await api.post('/users', { user: registerData });
+	localStorage.setItem('authToken', res.data.token);
+	api.defaults.headers.common.authorization = `Bearer ${res.data.token}`;
+	return resp.data.user;
+};
+
+export const verify = async () => {
+	const token = localStorage.getItem('authToken');
+	if (token) {
+		api.defaults.headers.common.authorization = `Bearer ${token}`;
+		const res = await api.get('/users/verify');
+		return res.data;
+	}
+	return null;
+};
+
+export const logout = () => {
+  localStorage.removeItem('authToken')
+	api.defaults.headers.common.authorization = null;
+};
