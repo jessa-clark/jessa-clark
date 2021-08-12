4092376cfc2a2f6ccccedc5f8fcfd4cafc2fdb7e
Jessa Clark <84357702+jessa-clark@users.noreply.github.com>
2021-08-12 12:23:07 -0400

    create projects.js

---
+++ client/src/Services/projects.js
@@ -0,0 +1,25 @@
+import api from "./apiConfig"
+
+export const getAllProjects = async () => {
+  const res = await api.get("/projects");
+  return res.data
+};
+
+export const getOneProject = async (id) => {
+  const res = await api.get(`/projects/${id}`);
+  return res.data
+};
+
+export const deleteProject = async (id) => {
+  await api.delete(`/projects/${id}`);
+};
+
+export const createProject = async (projectData) => {
+  const res = await api.post("/projects", { project: projectData });
+  return res.data
+};
+
+export const updateProject = async (id, projectData) => {
+  const res = await api.put(`/projects/${id}`, { project: projectData });
+  return res.data
+};
