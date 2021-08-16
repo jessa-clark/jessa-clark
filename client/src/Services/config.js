d38aacc73958f06e43944a46ce2defb7ee3deb36
Jessa Clark <84357702+jessa-clark@users.noreply.github.com>
2021-08-16 15:07:55 -0400

    netlify deploy

--- client/src/Services/config.js
+++ client/src/Services/config.js
@@ -1,7 +1,7 @@
 
 import axios from 'axios';
 
-const baseUrl = process.env.NODE_ENV === 'production' ? 'deploymentLink' : 'http://localhost:3000' ;
+const baseUrl = process.env.NODE_ENV === 'production' ? 'https://secret-fjord-93842.herokuapp.com' : 'http://localhost:3000' ;
 
 const api = axios.create({
 	baseURL: baseUrl,
