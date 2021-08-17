970b3216c244bd7598164bb7c9992e73c9a934ab
Jessa Clark <84357702+jessa-clark@users.noreply.github.com>
2021-08-17 16:57:20 -0400

    project details section on home update styling

--- client/src/Screens/Home/Home.jsx
+++ client/src/Screens/Home/Home.jsx
@@ -1,57 +1,52 @@
-import React, { useEffect, useState } from 'react'
-import HomeProjects from '../../Components/HomeProjects/HomeProjects'
-import MyLandingPage from '../../Components/Landing/Landing'
-import Layout from '../../Components/Layout/Layout'
-import hero1900 from '../Home/hero1900.png'
-import heromedia from '../Home/heromedia.png'
-import skills from '../Home/skills.png'
-import projects from '../Home/projects.png'
-import ContactCard from '../../Components/ContactCard/ContactCard'
-
-
+import React, { useEffect, useState } from "react";
+import HomeProjects from "../../Components/HomeProjects/HomeProjects";
+import MyLandingPage from "../../Components/Landing/Landing";
+import Layout from "../../Components/Layout/Layout";
+import hero1900 from "../Home/hero1900.png";
+import heromedia from "../Home/heromedia.png";
+import skills from "../Home/skills.png";
+import projects from "../Home/projects.png";
+import ContactCard from "../../Components/ContactCard/ContactCard";
 
 function Home() {
   const [width, setWidth] = useState(window.innerWidth);
   const mediumScreen = 1800;
   const smallScreen = 1640;
   const ipad = 1025;
-useEffect(() => { 
-  const handleWindowResize = () => setWidth(window.innerWidth);
-  window.addEventListener("resize", () => setWidth(window.innerWidth));
-  return () => window.removeEventListener("resize", handleWindowResize);
+  useEffect(() => {
+    const handleWindowResize = () => setWidth(window.innerWidth);
+    window.addEventListener("resize", () => setWidth(window.innerWidth));
+    return () => window.removeEventListener("resize", handleWindowResize);
   }, []);
 
   return (
-  <Layout>
-    <div className="landing-section">
+    <Layout>
+      <div className="landing-section">
         <div className="landing-container">
-
-      <img src={hero1900} />
-      {/* // )} */}
+          <img src={hero1900} />
+          {/* // )} */}
+        </div>
       </div>
-    </div>
-    <div className="skills-section">
-      <div className="skills-container">
-        <img src={skills} />
-      </div>
-    </div>
-    
-    <div>
-      <div className="notable-projects-text">
-      <h2>Notable Projects</h2>
+      <div className="skills-section">
+        <div className="skills-container">
+          <img src={skills} />
+        </div>
       </div>
 
-    <HomeProjects />
-    </div>
-
-
-
-    <ContactCard />
-
-
-
+      <div
+      style={{ backgroundImage : `url('${projects}')`}} className="projects-section">
+        <div className="notable-projects-text">
+          <h2>Notable Projects</h2>
+        </div>
+        <div className="home-projects-render">
+          <HomeProjects />
+        </div>
+      </div>
+    <div className="contact-section-container">
+      <ContactCard />
+      </div>
     </Layout>
-  )
+  );
 }
 
-export default Home
+export default Home;
