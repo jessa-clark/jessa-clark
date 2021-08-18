5d5f7b4fb013c09d0ff5a776c7892eb2a0ea03fc
Jessa Clark <84357702+jessa-clark@users.noreply.github.com>
2021-08-18 00:06:19 -0400

    skills & projects complete

--- client/src/Screens/Experience/Experience.jsx
+++ client/src/Screens/Experience/Experience.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
+import { useHistory } from 'react-router-dom'
 import './Experience.css'
 function Experience() {
+  const history = useHistory();
   return (
     <div className="experience-container">
       <div className="experience-section-home">
       <div className="experience-title">
         <h3>Skills and Abilities</h3>
       </div>
-      <button className="skills-button">
+      <button className="skills-button" onClick={() => history.push('../Skills')}>
       More
       </button>
       </div>
--- client/src/Screens/Skills/Skills.jsx
+++ client/src/Screens/Skills/Skills.jsx
@@ -1,27 +1,36 @@
 import "./Skills.css";
-import skills from "../Skills/Skills.png"
-import resume from "../Skills/Resume.png"
+import Layout from "../../Components/Layout/Layout"
+import skills from "../Skills/Skills.png";
+import resume from "../Skills/Resume.png";
 import { Link, NavLink } from "react-router-dom";
 
 function Skills() {
   return (
+    <Layout>
     <div className="skills-container">
       <div className="skills-section-skills">
         <div className="skills-page-title">
           <h2>Skills and Abilities</h2>
         </div>
-        <div className="resume-experience-section">
-          <div className="resume-experience-title">
-            <h2> Resume and Experience </h2>
-          </div>
-          <div className="resume-post">
-            <Link to={{pathname: "https://docs.google.com/document/d/14lYO-JF1AI_lZObza8wX7bG_EUkWsqGkldVCfP7N_SI/edit?usp=sharing"}} target="_blank">
+      </div>
+      <div className="resume-experience-section">
+        <div className="resume-experience-title">
+          <h2> Resume and Experience </h2>
+        </div>
+        <div className="resume-post">
+          <Link
+            to={{
+              pathname:
+                "https://docs.google.com/document/d/14lYO-JF1AI_lZObza8wX7bG_EUkWsqGkldVCfP7N_SI/edit?usp=sharing",
+            }}
+            target="_blank"
+          >
             <img src={resume}></img>
-            </Link>
-          </div>
+          </Link>
         </div>
       </div>
     </div>
+    </Layout>
   );
 }
 
