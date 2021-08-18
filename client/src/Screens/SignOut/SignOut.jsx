25a121ad92e5a8e29fa6e853199a8ec17ac93901
Jessa Clark <84357702+jessa-clark@users.noreply.github.com>
2021-08-18 11:30:53 -0400

    sign out issue fix

--- client/src/Screens/SignOut/SignOut.jsx
+++ client/src/Screens/SignOut/SignOut.jsx
@@ -1,22 +1,22 @@
 import { useEffect, useState } from "react";
-import { useHistory } from "react-router-dom";
+import { Redirect } from "react-router-dom";
 import { logout } from "../../Services/users";
 import Layout from "../../Components/Layout/Layout";
 
-export default function SignOut () {
+export default function SignOut() {
   const [signedOut, setSignedOut] = useState(false);
-  const history =useHistory()
+
   useEffect(() => {
     const signOutUser = async () => {
       const signOutBool = await logout();
       setSignedOut(signOutBool);
     };
     signOutUser();
-    history.pushState("/login")
   }, []);
 
   return (
     <Layout>
+      <Redirect to="/login" />
     </Layout>
-    )
-  }
+  );
+}
