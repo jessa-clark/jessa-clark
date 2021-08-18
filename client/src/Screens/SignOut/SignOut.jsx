1d51172c7cde047a593f9790dcee0266985db6f9
Jessa Clark <84357702+jessa-clark@users.noreply.github.com>
2021-08-18 11:20:04 -0400

    history push added to sign out

--- client/src/Screens/SignOut/SignOut.jsx
+++ client/src/Screens/SignOut/SignOut.jsx
@@ -1,22 +1,22 @@
 import { useEffect, useState } from "react";
-import { Redirect } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 import { logout } from "../../Services/users";
 import Layout from "../../Components/Layout/Layout";
 
 export default function SignOut () {
   const [signedOut, setSignedOut] = useState(false);
-
+  const history =useHistory()
   useEffect(() => {
     const signOutUser = async () => {
       const signOutBool = await logout();
       setSignedOut(signOutBool);
     };
     signOutUser();
+    history.pushState("/login")
   }, []);
 
   return (
     <Layout>
-    {signedOut ? <Redirect to="/login"/> : <h2>Signed out...</h2>}
     </Layout>
     )
   }
