3acdb70b3645127a38477e4aae0ecfdd547630a2
Jessa Clark <84357702+jessa-clark@users.noreply.github.com>
2021-08-24 21:13:49 -0400

    final push

--- client/src/Components/CommentTable/CommentTable.jsx
+++ client/src/Components/CommentTable/CommentTable.jsx
@@ -1,54 +1,54 @@
-import React, { useEffect, useState } from 'react'
-import { Link, useParams } from 'react-router-dom';
-import { getAllComments } from '../../Services/comments';
-import { verify } from '../../Services/users';
+// import React, { useEffect, useState } from 'react'
+// import { Link, useParams } from 'react-router-dom';
+// import { getAllComments } from '../../Services/comments';
+// import { verify } from '../../Services/users';
 
 
-function CommentTable(props) {
-const [comments, setComments] =useState([])
-const { id } = useParams();
-const [userExists, setUserExists] = useState(null);
+// function CommentTable(props) {
+// const [comments, setComments] =useState([])
+// const { id } = useParams();
+// const [userExists, setUserExists] = useState(null);
 
 
-  useEffect(() => {
-    const fetchComments = async () => {
-    const allComments = await getAllComments()
-    setComments(allComments)
-    };
-    fetchComments();
-  }, []);
+//   useEffect(() => {
+//     const fetchComments = async () => {
+//     const allComments = await getAllComments()
+//     setComments(allComments)
+//     };
+//     fetchComments();
+//   }, []);
 
-  useEffect(() => {
-    const checkSigned = async () => {
-      const valid = await verify();
-      setUserExists(valid ? true : false);
-    };
-    checkSigned();
-  }, []);
-
-
-  return (
-    <div>
-      <div className="comment-table-container">
-      {comments.map((comment) => {
-        return(
-          <div key={comment.id} className="comment-table">
-            <div className="comment-table-name">
-          <p>{comment.name}</p>
-          </div>
-          <div className="comment-table-comment">
-          <p>{comment.comment}</p>
-          </div>
-          {userExists ? (
-            <Link to={`/projects/${id}/comments/${comment.id}`}>edit comment</Link>
-        ) : null}
-          </div>  
-        )
-      })}
-      </div>
-      </div>
-  )
-}
+//   useEffect(() => {
+//     const checkSigned = async () => {
+//       const valid = await verify();
+//       setUserExists(valid ? true : false);
+//     };
+//     checkSigned();
+//   }, []);
 
 
-export default CommentTable
+//   return (
+//     <div>
+//       <div className="comment-table-container">
+//       {comments.map((comment) => {
+//         return(
+//           <div key={comment.id} className="comment-table">
+//             <div className="comment-table-name">
+//           <p>{comment.name}</p>
+//           </div>
+//           <div className="comment-table-comment">
+//           <p>{comment.comment}</p>
+//           </div>
+//           {userExists ? (
+//             <Link to={`/projects/${id}/comments/${comment.id}`}>edit comment</Link>
+//         ) : null}
+//           </div>  
+//         )
+//       })}
+//       </div>
+//       </div>
+//   )
+// }
+
+
+// export default CommentTable
