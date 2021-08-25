7f9c42acf7e9cb7c16b20b776a0aa1f5ac85ed25
Jessa Clark <84357702+jessa-clark@users.noreply.github.com>
2021-08-25 11:41:27 -0400

    responsive design padding fixes

--- client/src/Components/Landing/Landing.jsx
+++ client/src/Components/Landing/Landing.jsx
@@ -6,20 +6,14 @@ function MyLandingPage() {
   return (
     <div className="landing-container">
       <div className="title-container">
-      <div className="landing-title">
-        <p className="landing-name-1">Jessa Clark</p>
-      </div>
-      <div className="dev-title-container">
-        <p className="landing-developer">Software Engineer</p>
-      </div>
+          <p className="landing-name-1">Jessa Clark</p>
+          <p className="landing-developer">Software Engineer</p>
       </div>
       <div className="landing-image-container">
         <div className="landing-image">
           <img src={landing2} alt="popart woman computer" />
         </div>
-
       </div>
-
     </div>
   );
 }
